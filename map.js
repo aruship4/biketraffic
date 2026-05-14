bc05cc5fea3b4c5aba2091a0af5b43a5b57b695a
Arushi Patra <apatra@ucsd.edu>
2026-05-14 15:44:36 -0700

    Update map.js

--- map.js
+++ map.js
@@ -56,7 +56,7 @@ map.on('load', async () => {
 
     let jsonData;
     try {
-        const jsonurl = './bluebikes-stations.json';
+        const jsonurl = 'https://dsc106.com/labs/lab07/data/bluebikes-stations.json';
     
         // Await JSON fetch
         jsonData = await d3.json(jsonurl);
