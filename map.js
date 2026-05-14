d4a3bd3db654d28473015797d0577e38cff72da9
Arushi Patra <apatra@ucsd.edu>
2026-05-14 16:27:13 -0700

    updatepos

--- map.js
+++ map.js
@@ -1,72 +1,92 @@
-// Import Mapbox as an ESM module
 import mapboxgl from 'https://cdn.jsdelivr.net/npm/mapbox-gl@2.15.0/+esm';
 import * as d3 from 'https://cdn.jsdelivr.net/npm/d3@7.9.0/+esm';
 
+mapboxgl.accessToken = 'pk.xxx';
 
-// Check that Mapbox GL JS is loaded
-console.log('Mapbox GL JS Loaded:', mapboxgl);
-
-// Set your Mapbox access token here
-mapboxgl.accessToken = 'pk.eyJ1IjoiYXJ1c2hpNCIsImEiOiJjbXA0Y3plZWcwNW02MnFvcmJydHd3M3V2In0.Yur3wBTyqudu2YC0A2B2rw';
-
-// Initialize the map
 const map = new mapboxgl.Map({
-  container: 'map', // ID of the div where the map will render
-  style: 'mapbox://styles/mapbox/streets-v12', // Map style
-  center: [-71.09415, 42.36027], // [longitude, latitude]
-  zoom: 12, // Initial zoom level
-  minZoom: 5, // Minimum allowed zoom
-  maxZoom: 18, // Maximum allowed zoom
+  container: 'map',
+  style: 'mapbox://styles/mapbox/streets-v12',
+  center: [-71.09415, 42.36027],
+  zoom: 12,
 });
 
+const svg = d3.select('#map').select('svg');
+
 map.on('load', async () => {
-    //boston
-    map.addSource('boston_route', {
-        type: 'geojson',
-        data: './Existing_Bike_Network_2022.geojson',
-      });
 
-    map.addLayer({
-        id: 'bike-lanes',
-        type: 'line',
-        source: 'boston_route',
-        paint: {
-          'line-color': '#32D400',
-          'line-width': 5,
-          'line-opacity': 0.6,
-        },
-      });
+  // Boston
+  map.addSource('boston_route', {
+    type: 'geojson',
+    data: './Existing_Bike_Network_2022.geojson',
+  });
 
-    //cambridge
-    map.addSource('cambridge_route', {
-        type: 'geojson',
-        data: 'https://raw.githubusercontent.com/cambridgegis/cambridgegis_data/main/Recreation/Bike_Facilities/RECREATION_BikeFacilities.geojson',
-      });
-    
-    map.addLayer({
-        id: 'bike-lanes-cambridge',
-        type: 'line',
-        source: 'cambridge_route',
-        paint: {
-          'line-color': '#1E90FF', // blue so you can tell the difference
-          'line-width': 4,
-          'line-opacity': 0.6,
-        },
-      });
+  map.addLayer({
+    id: 'bike-lanes',
+    type: 'line',
+    source: 'boston_route',
+    paint: {
+      'line-color': '#32D400',
+      'line-width': 5,
+      'line-opacity': 0.6,
+    },
+  });
 
-    let jsonData;
-    try {
-        const jsonurl = 'https://dsc106.com/labs/lab07/data/bluebikes-stations.json';
-    
-        // Await JSON fetch
-        jsonData = await d3.json(jsonurl);
-    
-        console.log('Loaded JSON Data:', jsonData); // Log to verify structure
-    } catch (error) {
-        console.error('Error loading JSON:', error); // Handle errors
-    }
+  // Cambridge
+  map.addSource('cambridge_route', {
+    type: 'geojson',
+    data: 'https://raw.githubusercontent.com/cambridgegis/cambridgegis_data/main/Recreation/Bike_Facilities/RECREATION_BikeFacilities.geojson',
+  });
 
-    let stations = jsonData.data.stations;
-    console.log('Stations Array:', stations);
+  map.addLayer({
+    id: 'bike-lanes-cambridge',
+    type: 'line',
+    source: 'cambridge_route',
+    paint: {
+      'line-color': '#1E90FF',
+      'line-width': 4,
+      'line-opacity': 0.6,
+    },
+  });
 
-    });
+  let jsonData;
+  try {
+    jsonData = await d3.json(
+      'https://dsc106.com/labs/lab07/data/bluebikes-stations.json'
+    );
+  } catch (error) {
+    console.error(error);
+    return;
+  }
+
+  const stations = jsonData.data.stations;
+
+  const circles = svg
+    .selectAll('circle')
+    .data(stations)
+    .enter()
+    .append('circle')
+    .attr('r', 5)
+    .attr('fill', 'steelblue')
+    .attr('stroke', 'white')
+    .attr('opacity', 0.8);
+
+  function getCoords(station) {
+    const point = new mapboxgl.LngLat(+station.lon, +station.lat);
+    const { x, y } = map.project(point);
+    return { cx: x, cy: y };
+  }
+
+  function updatePositions() {
+    circles
+      .attr('cx', d => getCoords(d).cx)
+      .attr('cy', d => getCoords(d).cy);
+  }
+
+  updatePositions(); 
+
+  map.on('move', updatePositions);
+  map.on('zoom', updatePositions);
+  map.on('resize', updatePositions);
+  map.on('moveend', updatePositions);
+
+});
