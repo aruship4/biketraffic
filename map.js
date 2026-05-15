9a555dc1dbed00cff1ac830767f5711ad2dc107b
Arushi Patra <apatra@ucsd.edu>
2026-05-14 17:18:57 -0700

    Update map.js

--- map.js
+++ map.js
@@ -66,9 +66,6 @@ map.on('load', async () => {
     .enter()
     .append('circle')
     .attr('r', d => radiusScale(d.totalTraffic))
-    .attr('fill', 'steelblue')
-    .attr('stroke', 'white')
-    .attr('opacity', 0.8)
     .each(function (d) {
         d3.select(this)
             .append('title')
